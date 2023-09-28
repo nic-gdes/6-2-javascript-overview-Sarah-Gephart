a6666ff0681ac1bad3465473994ca598ad24f3c0
Sarah Gephart <sgephart@nic.edu>
2023-09-28 15:51:03 -0700

    Commit 09/28/23

--- javascript.js
+++ javascript.js
@@ -2,12 +2,50 @@
 
 // 1.2 Assignment: Variables
 
+var dudesName = "john";
+console.log(dudesName);
+var dudesAge = "25 Years Old";
+console.log(dudesAge);
+var dudesSex = "Male";
+console.log(dudesSex);
+var dudesNationality = "United Kingdom";
+console.log(dudesNationality);
+
 // 1.3 Assignment: Variable Declaration & Initialization
+
+var a = "Kitten";
+var b = 42;
+var c = true;
+var d = null;
+var e;
+
+console.log (a, b, c, d, e);
 
 // 1.4 Assignment: Complete your statements
 
+x = 2;
+y = 3;
+z = x + y;
+
 // 1.5 Assignment: Brackets, Braces and Parentheses
+
+var a = function(){
+    alert('Statement 1');
+    alert('Statement 2');
+}
+
+console.log(a());
 
 // 1.6 Assignment: Data Types
 
+var f = "Sarah"; //string
+var g = 20; //number
+var h = true; //boolean
+
 // 1.7 Assignment: Practicing JavaScript basics
+
+var a = 4;
+var b = 3;
+var c = 5;
+
+console.log(b*a/2);
